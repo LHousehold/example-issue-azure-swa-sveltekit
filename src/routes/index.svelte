5de7757bafee52f0e4fd0571c0a37619f0c78783
Luke Household <luke.household@gmail.com>
2021-11-22 12:51:49 -0500

    initial commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,23 @@
+<script>
+  import { onMount } from 'svelte';
+
+  console.log('script');
+
+  let x = "Default"; // runs server side
+
+  onMount(async () => {
+    console.log('mount');
+
+    x = "Updated with Javascript"; // runs client side
+  });
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+{x}
+
+<style>
+  p {
+    color: blue;
+  }
+</style>
